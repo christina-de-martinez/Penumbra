96d06ed660b402ffba86be52ef9ffdfea85853c3
Christina Houck <31422523+christinahouck@users.noreply.github.com>
2018-12-30 14:55:43 -0800

    Added Membership page with a web form that leads nowhere

--- scripts/main.js
+++ scripts/main.js
@@ -36,6 +36,7 @@ $(document).ready(function() {
 
 
 
+
 //  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 //    maxZoom: 18,
