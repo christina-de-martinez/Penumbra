7e887897b1faa90336d5e38315678e74b7d9b26b
Christina Houck <31422523+christinahouck@users.noreply.github.com>
2019-01-12 16:23:01 -0800

    Added Mobile Friendliness and fixed an issue with flickering JavaScript in the header.

--- scripts/main.js
+++ scripts/main.js
@@ -1,48 +1,30 @@
 $(document).ready(function() {
+  $(function(){
+    $('nav').data('size','big');
+  });
 
-  scrollFunction();
-
-//  $(".item").hover(function(){
-//    $(this).css("background-color","#E0552F;");
-//  }, function(){
-//    $(this).css("background-color","#8F8D87");
-//  });
-
-  window.onscroll = function(){
-    scrollFunction();
-  };
-
-  function scrollFunction() {
-    if (document.body.scrollTop >= 120 || document.documentElement.scrollTop >= 120) {
-      document.getElementById("navbar").className = "smallnav";
-      document.getElementsByClassName("item").className = "smallitem";
-      $(".item").hover(function(){
-        $(this).css("height","40px");
-      }, function(){
-        $(this).css("height","40px");
-      });
-
-    } else {
-      document.getElementById("navbar").className = "bignav";
-      document.getElementsByClassName("item").className = "bigitem";
-      $(".item").hover(function(){
-        $(this).css("height","90px");
-      }, function(){
-        $(this).css("height","90px");
-      });
-    }
+  $(window).scroll(function(){
+    if($(document).scrollTop() > 50)
+  {
+      if($('nav').data('size') == 'big')
+      {
+          $('nav').data('size','small');
+          $('nav').stop().animate({
+              height:'40px'
+          },900);
+      }
   }
-
-
-
-
-
-//  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-//    maxZoom: 18,
-//}).addTo(map);
+  else
+    {
+      if($('nav').data('size') == 'small')
+        {
+          $('nav').data('size','big');
+          $('nav').stop().animate({
+              height:'90px'
+          },900);
+        }
+    }
+  });
 
   console.log('If you\'re seeing this, say hi! https://github.com/christinahouck');
 });
-
-// pk.eyJ1IjoiY2hyaXN0aW5hbWFydGluZXoiLCJhIjoiY2pxNXp2ZGNvMDhrNDQ5bGhlMnVtMXgxNSJ9.0Pr9hMVJfLv67rVM-pl9Ug
